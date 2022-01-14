f713d7aaa2564bc631a6057be58d97176e33c8bb
NidhitheCoder <nidhpv@gmail.com>
2022-01-14 23:58:43 +0530

    style: styling changed and events

---
+++ animated-website/script.js
@@ -0,0 +1,13 @@
+let featuresections = [...document.querySelectorAll('.features')];
+featuresections.map(feature => {
+  console.log(feature)
+  // feature.addEventListner('onHover', () => {
+  //   console.log('Hovered')
+  // })
+})
+
+
+
+// featuresections.map( item => {
+// console.log(item)
+// })
