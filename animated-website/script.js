36ca355a0dc59f4c5614e94bebb36e0093023ca6
NidhitheCoder <nidhpv@gmail.com>
2022-02-10 23:54:36 +0530

    feat: set timeout and interval

--- animated-website/script.js
+++ animated-website/script.js
@@ -9,3 +9,22 @@ const init = function() {
   cssScrollSnapPolyfill();
 };
 init();
+
+
+let length = array_list.length;
+let i = 0;
+getData();
+
+function getData() {
+  if (i < length) {
+    content.innerText = `${array_list[i].title}`;
+    profileName.innerText = `${array_list[i].name}`;
+    profileImage.innerHTML = `<img src="${array_list[i].image}" alt="">`;
+    userLocation.innerText = `${array_list[i].location}`;
+    i++;
+    setInterval(getData, 4000);
+  } else {
+    i = 0;
+    setInterval(getData, 4000);
+  }
+}
