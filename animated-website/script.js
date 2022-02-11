831d158180ba984679f2581409b4cdbed32fab51
NidhitheCoder <nidhpv@gmail.com>
2022-02-11 19:05:11 +0530

    feat: set a custom sample carousal

--- animated-website/script.js
+++ animated-website/script.js
@@ -10,21 +10,41 @@ const init = function() {
 };
 init();
 
-
-let length = array_list.length;
+let sampleArr = [
+  {
+  title: 'Title 1',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, harum!'
+},
+{
+  title: 'Title 2',
+  content: 'psum dolor sit amet, consectetur adipisicing elit. At, harum!'
+},
+{
+  title: 'Title 3',
+  content: 'ipsum dolor sit amet, consectetur adipisicing elit. At, harum!'
+},
+{
+  title: 'Title 4',
+  content: 'hello ipsum dolor sit amet, consectetur adipisicing elit. At, harum!'
+},
+];
+let length = sampleArr.length;
 let i = 0;
-getData();
 
 function getData() {
   if (i < length) {
-    content.innerText = `${array_list[i].title}`;
-    profileName.innerText = `${array_list[i].name}`;
-    profileImage.innerHTML = `<img src="${array_list[i].image}" alt="">`;
-    userLocation.innerText = `${array_list[i].location}`;
+    let intervalId;
+    const title = document.getElementById('title');
+    const content = document.getElementById('content');
+    title.textContent = sampleArr[i].title;
+    content.textContent = sampleArr[i].content;
     i++;
-    setInterval(getData, 4000);
+    intervalId = setInterval(getData, 4000);
+    setTimeout(() => clearInterval(intervalId), 4000);
   } else {
     i = 0;
-    setInterval(getData, 4000);
+    getData();
   }
-}
+};
+
+getData();
