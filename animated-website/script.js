2fa18d73864613a8746c6e8b432a9c3466e85f98
NidhitheCoder <nidhpv@gmail.com>
2022-03-13 00:33:27 +0530

    feat: implement drag and drop of image tiles

--- animated-website/script.js
+++ animated-website/script.js
@@ -118,3 +118,22 @@ ringContainer.addEventListener('click', () => {
   })
 
 })
+
+//  drag and drop
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  if (!ev.target.innerText) {
+  ev.target.appendChild(document.getElementById(data));
+  } else {
+    alert('Not allowed');
+  }
+}
