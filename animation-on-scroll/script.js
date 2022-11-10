461e2b266e2fff50ec7cb5b450299e335b9a0bd0
Nidheesh PV <nidheesh@vonnue.com>
2022-11-10 23:57:23 +0530

    fix: styles in animated cards

--- animation-on-scroll/script.js
+++ animation-on-scroll/script.js
@@ -4,8 +4,13 @@ window.addEventListener("scroll", checkboxes);
 checkboxes();
 function checkboxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
+  boxes.forEach((box, index) => {
+    let boxTop;
+    if (box[index + 1]) {
+      boxTop = box[index + 1].getBoundingClientRect().top;
+    } else {
+      boxTop = box.getBoundingClientRect().top;
+    }
 
     if (boxTop < triggerBottom) {
       box.classList.add("show");
