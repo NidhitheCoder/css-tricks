be20858539f64c159ed570fe2422b6b1245aa01b
Nidheesh PV <nidheesh@vonnue.com>
2023-01-26 23:36:14 +0530

    feat: install preset env

--- post-css/postcss.config.js
+++ post-css/postcss.config.js
@@ -1,9 +1,13 @@
-const cssnano = require('cssnano');
+const cssnano = require("cssnano");
+const postCssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
   plugins: [
     cssnano({
       preset: "default",
+    }),
+    postCssPresetEnv({
+      stage: 1,
     }),
   ],
 };
