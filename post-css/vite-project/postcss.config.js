7736030409a01f151efa57188c90064bed0a0518
Nidheesh PV <nidheesh@vonnue.com>
2023-01-28 23:58:53 +0530

    feat: vita project running

---
+++ post-css/vite-project/postcss.config.js
@@ -0,0 +1,15 @@
+import cssnano from 'cssnano';
+
+const cssnano = require("cssnano");
+const postCssPresetEnv = require("postcss-preset-env");
+
+module.exports = {
+  plugins: [
+    cssnano({
+      preset: "default",
+    }),
+    postCssPresetEnv({
+      stage: 1,
+    }),
+  ],
+};
